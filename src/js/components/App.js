654cc3159de055b3a8acf60a897f195f71ce89c6
himanshu <himanshu131194@.outlookcom>
2021-03-17 15:38:55 +0530

    added

---
+++ src/js/components/App.js
@@ -0,0 +1,15 @@
+import React, {Component} from 'react'
+class App extends Component{
+      shouldComponentUpdate(){
+         return false;
+      }
+      render(){
+      	 return(
+      	 	<div>
+                <h1>Hi, this is my react project</h1>
+            </div>
+      	 )
+      }
+}
+
+export default App;
