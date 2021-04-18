5e9d1fd2d41e051a94a07f64bfdc1ae72efd7bef
himasnhu savita <himanshu131194@users.noreply.github.com>
2021-04-18 14:43:34 +0530

    added

--- src/js/components/App.js
+++ src/js/components/App.js
@@ -6,8 +6,8 @@ class App extends Component{
       render(){
       	 return(
       	 	<div>
-                <h1>Hi, this is my react project</h1>
-            </div>
+                        <h1>Hi, this is my react project</h1>
+                  </div>
       	 )
       }
 }
